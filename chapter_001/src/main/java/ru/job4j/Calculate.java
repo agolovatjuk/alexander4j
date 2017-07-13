b7182297c94bb8b8dbf1949c2224ea89c564b440
Alexander Golovatyuk <a.golovatjuk@gmail.com>
2017-07-13 14:40:56 +0500

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
-
+/**
+ * Class для вычисления арифметичкеских операций + - / *
+ * @author agololvatjuk
+ * @version $Id$
+ * @since 13.07.2017
+ */
 public class Calculate{
+    /**
+     * Проверим работоспособность
+     * @param args аргументы командной строки
+     * @return void
+     */
     public static void main(String[] args){
         System.out.println("Hello, world");
     }
