aeaf57505ddd7185d238ccc0f8c0c8a2ec6d827a
golo <a.golovatjuk@gmail.com>
2017-10-17 00:50:39 +0500

    final Cell position in Figure

--- chapter_002/src/main/java/ru/job4j/chesstest/Figure.java
+++ chapter_002/src/main/java/ru/job4j/chesstest/Figure.java
@@ -3,7 +3,7 @@ package ru.job4j.chesstest;
 abstract public class Figure {
     final String color;
     final String name;
-    Cell position;
+    final Cell position;
 
     Figure(String name, Cell position, String color) {
         this.name = name;
