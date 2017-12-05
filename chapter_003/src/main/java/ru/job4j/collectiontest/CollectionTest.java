7b40c9e8a4f6f0560eba486ca29c770bb41f4459
Alexander Golovatyuk <a.golovatjuk@gmail.com>
2017-12-05 13:56:57 +0500

    2.[LinkedList] Тестирование производительности коллекций.[#29173]

--- chapter_003/src/main/java/ru/job4j/collectiontest/CollectionTest.java
+++ chapter_003/src/main/java/ru/job4j/collectiontest/CollectionTest.java
@@ -25,37 +25,39 @@ public class CollectionTest {
     }
 
     public long delete(Collection<String> collection, int amount) {
-        int[] idx = new int[amount];
-        Random r = new Random();
-        for (int i = 0; i < amount; i++) {
-            idx[i] = r.nextInt(this.data.length);
-        }
+        String[] idx = new String[amount];
+        System.arraycopy(this.data, 0, idx, 0, amount);
+//        Random r = new Random();
+//        for (int i = 0; i < amount; i++) {
+//            idx[i] = r.nextInt(this.data.length);
+//        }
 
         Clock clock = Clock.systemUTC();
         long t = clock.millis();
         for (int i = 0; i < amount; i++) {
-            collection.remove(this.data[idx[i]]);
+//            collection.remove(this.data[idx[i]]);
+            collection.remove(idx[i]);
         }
         return clock.millis() - t;
     }
 
     public static void main(String[] args) {
         int nAdd = 400000;
-        int nDel = 2000;
+        int nDel = 4000;
         CollectionTest tc = new CollectionTest();
 
         LinkedList<String> lstr = new LinkedList<>();
         ArrayList<String>  astr = new ArrayList<>();
         TreeSet<String>    tstr = new TreeSet<>();
 
-        System.out.println("\nTest ArrayList");
-        System.out.printf("Add %d elapsed time, msec: %d", nAdd, tc.add(astr, nAdd));
-        System.out.printf("  Del %d elapsed time, msec: %d", nDel, tc.delete(astr, nDel));
-        System.out.println("\nTest TreeSet");
-        System.out.printf("Add %d elapsed time, msec: %d", nAdd, tc.add(tstr, nAdd));
-        System.out.printf("  Del %d elapsed time, msec: %d", nDel, tc.delete(tstr, nDel));
         System.out.println("\nTest LinkedList");
         System.out.printf("Add %d elapsed time, msec: %d", nAdd, tc.add(lstr, nAdd));
         System.out.printf("  Del %d elapsed time, msec: %d", nDel, tc.delete(lstr, nDel));
+        System.out.println("\nTest TreeSet");
+        System.out.printf("Add %d elapsed time, msec: %d", nAdd, tc.add(tstr, nAdd));
+        System.out.printf("  Del %d elapsed time, msec: %d", nDel, tc.delete(tstr, nDel));
+        System.out.println("\nTest ArrayList");
+        System.out.printf("Add %d elapsed time, msec: %d", nAdd, tc.add(astr, nAdd));
+        System.out.printf("  Del %d elapsed time, msec: %d", nDel, tc.delete(astr, nDel));
     }
 }
