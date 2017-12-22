a43bb3e6b5faa69f25b084aad860c1a6c0a63366
golo <a.golovatjuk@gmail.com>
2017-12-23 00:09:02 +0500

    [#34681] SortUserTest refactored

--- chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
@@ -2,11 +2,12 @@ package ru.job4j.sorting;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.Random;
 
+import static java.lang.String.format;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -15,13 +16,16 @@ public class SortUserTest {
     @Test
     public void whenUserSetSort() {
         List<User> users = new ArrayList<>();
-        users.addAll(Arrays.asList(
-                new User("User1", 10), new User("User2", 5), new User("User3", 7)));
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++) {
+            int x = rnd.nextInt(100);
+            users.add(new User(format("Name_%d", x), x));
+        }
         Set<User> userset = new SortUser().sort(users);
-        List<User> usr = new ArrayList<>();
-        usr.addAll(userset);
+        List<User> usr = new ArrayList<>(userset);
         for (int i = 1; i < usr.size(); i++) {
             assertThat(usr.get(i - 1).compareTo(usr.get(i)), is(-1));
         }
+        System.out.println(usr);
     }
 }
