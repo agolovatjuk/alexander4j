76dbdfea0aaa172790798911c2481b9a548a3929
golo <a.golovatjuk@gmail.com>
2017-12-22 08:57:15 +0500

    в тестах equalTo() заменён на is()

--- chapter_003/src/test/java/ru/job4j/collectiontest/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/collectiontest/ConvertListTest.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class ConvertListTest {
@@ -68,6 +67,6 @@ public class ConvertListTest {
         List<Integer> result = convertList.convert(list);
         List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        assertThat(result, equalTo(expected));
+        assertThat(result, is(expected));
     }
 }
