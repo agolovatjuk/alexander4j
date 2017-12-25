a5714cbc600639e7997ad6f996869d27dd85b36e
golo <a.golovatjuk@gmail.com>
2017-12-25 20:54:14 +0500

    [#34681] SortUser refactored

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -8,18 +8,13 @@ import java.util.Arrays;
 
 public class SortUser {
     public Set<User> sort(List<User> users) {
-        Set<User> userset = new TreeSet<>();
-        userset.addAll(users);
-        return userset;
+        return new TreeSet<>(users);
     }
 
     public static void main(String[] args) {
-
-        List<User> users = new ArrayList<>();
-        users.addAll(Arrays.asList(
+        List<User> users = new ArrayList<>(Arrays.asList(
                 new User("User1", 10), new User("User2", 5), new User("User3", 7)));
         System.out.println(users);
-        Set<User> userset = new SortUser().sort(users);
-        System.out.println(userset);
+        System.out.println(new SortUser().sort(users));
     }
 }
