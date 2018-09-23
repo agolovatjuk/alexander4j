a480ee30301aecce2c0d615aa992b4bb9afd50a9
golo <a.golovatjuk@gmail.com>
2018-09-23 23:39:00 +0500

    8. Реализовать собственную структуру данных - HashMap[#58154]

---
+++ junior_001/src/main/java/ru/job4j/map/UserHashMap.java
@@ -0,0 +1,207 @@
+package ru.job4j.map;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class UserHashMap<K, V> {
+
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    static final int DEFAULT_INITIAL_CAPACITY = 8;
+
+    final float loadfactor;
+    int size;
+    int modCount;
+
+    Node<K, V>[] table;
+
+    static class Node<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return key + ", " + value;
+        }
+    }
+
+    public UserHashMap() {
+        this(DEFAULT_INITIAL_CAPACITY);
+    }
+
+    public UserHashMap(int capacity) {
+        this.loadfactor = DEFAULT_LOAD_FACTOR;
+        this.table = new Node[Math.min(capacity, Integer.MAX_VALUE)];
+    }
+
+    public UserHashMap(int capacity, float loadFactor) {
+        this.loadfactor = loadFactor;
+        this.table = new Node[Math.min(capacity, Integer.MAX_VALUE)];
+    }
+
+    public int hash(K key) {
+        double a = (Math.sqrt(5) - 1) / 2;
+        double x = a * key.hashCode() % 1 * table.length;
+        return (int) Math.abs(x);
+    }
+
+    public boolean insert(K key, V value) {
+        return this.put(key, value);
+    }
+
+    public boolean put(K key, V value) {
+        if ((float) this.size / table.length >= this.loadfactor) {
+            resize(table.length * 2);
+        }
+        remove(key);
+        Node<K, V> node = new Node<>(key, value);
+        int idx = hash(key);
+        if (table[idx] != null && !table[idx].key.equals(key)) {
+            node.next = table[idx];
+        }
+        table[idx] = node;
+        size++;
+        modCount++;
+        return true;
+    }
+
+    public V get(K key) {
+        if (table[hash(key)] != null) {
+            Node node = table[hash(key)];
+            while (node.next != null && !node.key.equals(key)) {
+                node = node.next;
+            }
+            return node.key.equals(key) ? (V) node.value : null;
+        }
+        return null;
+    }
+
+    public boolean delete(K key) {
+        return remove(key);
+    }
+
+    public boolean remove(K key) {
+        boolean value = false;
+        Node<K, V> p = table[hash(key)];
+        if (p != null) {
+            Node<K, V> e = p.next;
+            if (key.equals(p.key)) {
+                table[hash(key)] = p.next;
+                p = null;
+                value = true;
+            } else if (e != null) {
+                do {
+                    if (key.equals(e.key)) {
+                        p.next = e.next;
+                        e = null;
+                        value = true;
+                        break;
+                    }
+                    p = p.next;
+                    e = e.next;
+                } while (e != null);
+            }
+            if (value) {
+                size--;
+                modCount++;
+            }
+        }
+        return value;
+    }
+
+    public boolean resize(int newsize) {
+        if (newsize >= Integer.MAX_VALUE) {
+            return false;
+        }
+        UserHashMap<K, V> tmpHashMap = new UserHashMap<>(newsize);
+        populateUserHashMap(tmpHashMap);
+        return true;
+    }
+
+    public boolean resize(int newsize, float loadFactor) {
+        if (newsize >= Integer.MAX_VALUE) {
+            return false;
+        }
+        UserHashMap<K, V> tmpHashMap = new UserHashMap<>(newsize, loadFactor);
+        populateUserHashMap(tmpHashMap);
+        return true;
+    }
+
+    private void populateUserHashMap(UserHashMap<K, V> tmpHashMap) {
+        Iterator it = getItemIterator();
+        Node<K, V> n;
+        while (it.hasNext()) {
+            n = (Node<K, V>) it.next();
+            tmpHashMap.put(n.key, n.value);
+        }
+        this.table = tmpHashMap.table;
+        this.size = tmpHashMap.size;
+        this.modCount = tmpHashMap.modCount;
+    }
+
+    public Iterator<K> getKeyIterator() {
+        return new KeyIterator();
+    }
+
+    public Iterator<Node> getItemIterator() {
+        return new ItemIterator();
+    }
+
+    abstract class MyHashIterator {
+        int expectedModCount;
+        int index;
+        Node<K, V> nextNode;
+
+        MyHashIterator() {
+            expectedModCount = modCount;
+            nextNode = null;
+            index = 0;
+            if (table != null && size > 0) {
+                while (index < table.length && nextNode == null) {
+                    nextNode = table[index++];
+                }
+            }
+        }
+
+        public boolean hasNext() {
+            return nextNode != null;
+        }
+
+        public Node<K, V> nextNode() {
+            Node<K, V> e = nextNode;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            nextNode = e.next;
+            if (nextNode == null && table != null) {
+                while (index < table.length && nextNode == null) {
+                    nextNode = table[index++];
+                }
+            }
+            return e;
+        }
+    }
+
+    public final class KeyIterator extends MyHashIterator implements Iterator<K> {
+        @Override
+        public K next() {
+            return nextNode().key;
+        }
+    }
+
+    public final class ItemIterator extends MyHashIterator implements Iterator<Node> {
+        @Override
+        public Node next() {
+            return nextNode();
+        }
+    }
+}
---
+++ junior_001/src/test/java/ru/job4j/map/UserHashMapTest.java
@@ -0,0 +1,138 @@
+package ru.job4j.map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNull;
+
+public class UserHashMapTest {
+
+    UserHashMap<String, String> userHashMap;
+
+    @Before
+    public void setUp() throws Exception {
+         userHashMap = new UserHashMap<>(4);
+    }
+
+    @Test
+    public void testWhenInsertAndResizeDueToLoadFactorThen() {
+        userHashMap.insert("test1", "Data1");
+        userHashMap.insert("test2", "Data2");
+        userHashMap.insert("test3", "Data3");
+        assertThat(userHashMap.table.length, is(4));
+        userHashMap.insert("test4", "Data4");
+        boolean res = userHashMap.insert("test5", "Data5");
+        assertThat(userHashMap.table.length, is(8));
+        assertThat(res, is(true));
+    }
+
+    @Test
+    public void whenRemoveInBigChainThen() {
+        int count = 100;
+        boolean res;
+        for (int i = 0; i < count; i++) {
+            userHashMap.insert("Key_" + i, "Data_" + i);
+        }
+        res = userHashMap.resize(10, 10f); // shrink table, increase each bin
+        assertThat(res, is(true));
+        assertThat(userHashMap.size, is(count));
+        for (int i = 0; i < count; i++) {
+            userHashMap.remove("Key_" + i);
+        }
+        assertThat(userHashMap.size, is(0));
+    }
+
+    @Test
+    public void whenGetThen() {
+        int count = 100;
+        UserHashMap<String, Integer> map = new UserHashMap<>();
+        for (int i = 0; i < count; i++) {
+            boolean res = map.insert("Key" + Integer.toString(i), i);
+            assertThat(res, is(true));
+        }
+        for (int i = 0; i < map.size; i++) {
+            Integer res = map.get("Key" + i);
+            assertThat(res, is(i));
+        }
+        Integer resInt = map.get("No_data");
+        assertNull(resInt);
+    }
+
+    @Test
+    public void whenDeleteThen() {
+        int count = 100;
+        UserHashMap<Integer, String> map = new UserHashMap<>();
+        for (int i = 0; i < count; i++) {
+            boolean res = map.insert(i, "Data_" + i);
+            assertThat(res, is(true));
+        }
+        assertThat(map.size, is(count));
+        assertThat(map.delete(111111), is(false));
+        int sz = map.size;
+        for (int i = 0; i < sz; i++) {
+            boolean resDel = map.delete(i);
+            assertThat(resDel, is(true));
+        }
+        assertThat(map.size, is(0));
+    }
+
+    @Test
+    public void whetIteratorsThen() {
+        int count = 100;
+        UserHashMap<String, String> map = new UserHashMap<>(10);
+        for (int i = 0; i < count; i++) {
+            boolean resIns = map.insert("Key_" + i, "Data_" + i);
+            assertThat(resIns, is(true));
+        }
+        Iterator<String> ikey = map.getKeyIterator();
+        while (ikey.hasNext()) {
+            String k = ikey.next();
+            String value = k.substring(3);
+            assertThat(map.get(k), is("Data" + value));
+        }
+        Iterator<UserHashMap.Node> ititems = map.getItemIterator();
+        while (ititems.hasNext()) {
+            UserHashMap.Node<String, String> node = ititems.next();
+            assertThat(map.get((String) node.key), is(node.value));
+            assertThat(node.toString(), is(node.key + ", " + node.value));
+        }
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenEditIteratorThen() {
+        userHashMap.insert("A", "Data_A");
+        userHashMap.insert("B", "Data_B");
+        Iterator<String> ikey = userHashMap.getKeyIterator();
+        ikey.next();
+        userHashMap.insert("C", "Data_C");
+        ikey.next(); // ConcurrentModificationException
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNoSuchElementThen() {
+        userHashMap.insert("X001", "Secret");
+        Iterator<String> ikey = userHashMap.getKeyIterator();
+        ikey.next();
+        ikey.next(); // NoSuchElementException
+    }
+
+    @Test
+    public void whenMaxValIntegerResizeThen() {
+        boolean res;
+        int oldsize;
+        oldsize = userHashMap.size;
+        res = userHashMap.resize(Integer.MAX_VALUE);
+        assertThat(res, is(false));
+        assertThat(oldsize, is(userHashMap.size));
+        oldsize = userHashMap.size;
+        res = userHashMap.resize(Integer.MAX_VALUE, 0.75f);
+        assertThat(res, is(false));
+        assertThat(oldsize, is(userHashMap.size));
+    }
+}
