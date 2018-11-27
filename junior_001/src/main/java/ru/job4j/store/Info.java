37ab03d5a482ae72044d5c0ab46ccb342638d8e7
golo <a.golovatjuk@gmail.com>
2018-11-27 21:24:00 +0500

    [#85992] упростил проверку по ключам, убрал exception

--- junior_001/src/main/java/ru/job4j/store/Info.java
+++ junior_001/src/main/java/ru/job4j/store/Info.java
@@ -1,58 +1,50 @@
 package ru.job4j.store;
 
-import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashSet;
 
 class Info {
 
-    HashMap<String, Integer> info = new HashMap<>();
-    Map<Integer, String> prev, curr;
+    HashMap<Integer, String> prev, curr;
+    HashMap<String, Integer> report;
 
     protected Info(List<Store.User> previous, List<Store.User> current) {
-        info.put("Added", 0);
-        info.put("Changed", 0);
-        info.put("Deleted", 0);
         prev = new HashMap<>();
         curr = new HashMap<>();
+        report = new HashMap<>();
         // TODO lambda how to
-        for (Store.User user: current) {
+        for (Store.User user : current) {
             curr.put(user.id, user.name);
         }
-        for (Store.User user: previous) {
+        for (Store.User user : previous) {
             prev.put(user.id, user.name);
         }
         calculateDifference();
     }
 
     private void calculateDifference() {
-        Set<Integer> prvKeys = new TreeSet<>(prev.keySet());
-        Set<Integer> curKeys = new TreeSet<>(curr.keySet());
-        Set<Integer> addedKeys;
-        // added Entries
-        if (curKeys.removeAll(prvKeys)) {
-            info.put("Added", curKeys.size());
-            addedKeys = new TreeSet<>(curKeys);
-        } else {
-            throw new RuntimeException("Error to calculate Added Entries");
-        }
-        // deleted Entries
-        curKeys = new TreeSet<>(curr.keySet());
-        if (prvKeys.removeAll(curKeys)) {
-            info.put("Deleted", prvKeys.size());
-        } else {
-            throw new RuntimeException("Error to calculate Deleted Entries");
-        }
-        // changed Entries
-        curKeys.removeAll(addedKeys);
-        int cntChanged = 0;
-        for (Integer key: curKeys) {
-            if (!prev.get(key).equals(curr.get(key))) {
-                cntChanged++;
+        int cntAdded = 0;
+        int cntEdited = 0;
+        int cntDeleted = 0;
+        HashSet<Integer> totalKeys = new HashSet<>();
+
+        totalKeys.addAll(prev.keySet());
+        totalKeys.addAll(curr.keySet());
+
+        for (Integer key : totalKeys) {
+            if (prev.containsKey(key) & curr.containsKey(key)) {
+                if (!prev.get(key).equals(curr.get(key))) {
+                    cntEdited++;
+                }
+            } else if (prev.containsKey(key) & !curr.containsKey(key)) {
+                cntDeleted++;
+            } else if (!prev.containsKey(key) & curr.containsKey(key)) {
+                cntAdded++;
             }
         }
-        info.put("Changed", cntChanged);
+        report.put("Added", cntAdded);
+        report.put("Changed", cntEdited);
+        report.put("Deleted", cntDeleted);
     }
 }
--- junior_001/src/test/java/ru/job4j/store/InfoTest.java
+++ junior_001/src/test/java/ru/job4j/store/InfoTest.java
@@ -16,7 +16,7 @@ public class InfoTest {
     Info info;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         for (int i = 0; i < 5; i++) {
             user = new Store.User();
             user.id = i;
@@ -34,9 +34,9 @@ public class InfoTest {
     public void whenPreviousEqualCurrentThen() {
         // see setUp()
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(0));
-        assertThat(info.info.get("Changed"), is(0));
-        assertThat(info.info.get("Deleted"), is(0));
+        assertThat(info.report.get("Added"), is(0));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(0));
     }
 
     @Test
@@ -48,9 +48,9 @@ public class InfoTest {
             current.add(user);
         }
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(4));
-        assertThat(info.info.get("Changed"), is(0));
-        assertThat(info.info.get("Deleted"), is(0));
+        assertThat(info.report.get("Added"), is(4));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(0));
     }
 
     @Test
@@ -58,9 +58,9 @@ public class InfoTest {
         current.remove(4);
         current.remove(0);
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(0));
-        assertThat(info.info.get("Changed"), is(0));
-        assertThat(info.info.get("Deleted"), is(2));
+        assertThat(info.report.get("Added"), is(0));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(2));
     }
 
     @Test
@@ -68,44 +68,44 @@ public class InfoTest {
         current.get(2).name = "ChangeN 111";
         current.get(0).name = "ChangeN 222";
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(0));
-        assertThat(info.info.get("Changed"), is(2));
-        assertThat(info.info.get("Deleted"), is(0));
+        assertThat(info.report.get("Added"), is(0));
+        assertThat(info.report.get("Changed"), is(2));
+        assertThat(info.report.get("Deleted"), is(0));
     }
 
     @Test
     public void whenInfoThen() {
-        for (int i = 0; i <= 4; i++) {
+        info = new Store().diff(previous, current);
+        assertThat(info.report.get("Added"), is(0));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(0));
+
+        for (int i = 2; i <= 5; i++) { // added 4 Entries - 2, 3, 4, 5
             user = new Store.User();
             user.id = i * 100;
             user.name = "User_" + i * 100;
             current.add(user);
         }
 
-        for (int i = 1; i <= 2; i++) {
-            current.remove(i);
-        }
+        info = new Store().diff(previous, current);
+        assertThat(info.report.get("Added"), is(4));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(0));
+
+        current.remove(1);
+        current.remove(2);
 
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(4));
-        assertThat(info.info.get("Changed"), is(0));
-        assertThat(info.info.get("Deleted"), is(2));
+        assertThat(info.report.get("Added"), is(4));
+        assertThat(info.report.get("Changed"), is(0));
+        assertThat(info.report.get("Deleted"), is(2));
 
         current.get(1).name = "Changed01 in current";
         current.get(2).name = "Changed02 in current";
 
         info = new Store().diff(previous, current);
-        assertThat(info.info.get("Added"), is(4));
-        assertThat(info.info.get("Changed"), is(2));
-        assertThat(info.info.get("Deleted"), is(2));
+        assertThat(info.report.get("Added"), is(4));
+        assertThat(info.report.get("Changed"), is(2));
+        assertThat(info.report.get("Deleted"), is(2));
     }
-
-    @Test(expected = RuntimeException.class)
-    public void whenNullEntriesThen() {
-        List<Store.User> prv = new ArrayList<>();
-        List<Store.User> cur = new ArrayList<>();
-        info = new Store().diff(previous, cur);
-        info = new Store().diff(prv, current);
-    }
-
 }
