a049f96f3fb4ee35cf1266f75e276d2b1ed136bc
golo <a.golovatjuk@gmail.com>
2018-12-14 23:55:24 +0500

    Generic, reflection, tests fixed

--- junior_001/src/test/java/ru/job4j/generic/StackTest.java
+++ junior_001/src/test/java/ru/job4j/generic/StackTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class StackTest {
+
+    @Test
+    public void whenCreateStackThen() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+        Stack stringStack = new Stack(10);
+        stringStack.push("A");
+        assertThat(stringStack.size(), is(1));
+        String result = stringStack.pop();
+        assertThat(result, is("A"));
+        assertThat(stringStack.size(), is(0));
+    }
 
 }
